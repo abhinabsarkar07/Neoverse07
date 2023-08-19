ad5926e70318f80c95a6123c7c43a95bc9e5694a
Abhinab Sarkar <abhinabsarkar07@gmail.com>
2023-08-19 21:47:03 +0530

    created navbar and footer

---
+++ neoverse/src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div style={{ backgroundColor:'black',padding: '15px', textAlign: 'center' }}>
+      <p style={{fontSize:"18px"}}>&copy; 2023 Abhinab Sarkar. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
