354af7cc4bc0ad28bd42022c08b2c47116663fd8
Abhinab Sarkar <abhinabsarkar07@gmail.com>
2023-09-20 21:46:33 +0530

    Created maintenance page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
-import Home from './Pages/Home/Home';
+// import Home from './Pages/Home/Home';
 import './App.css';
+import Maintenance from "./Pages/Home/Maintenance";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/" element={<Home />} /> */}
+        <Route exact path="/error" element={<Maintenance />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Pages/Home/Maintenance.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../Styles/Maintenance.css";
+const Maintenance = () => {
+  return (
+    <div className="maintenance-page">
+      <div className="maintenance-content">
+        <h1>Our site is currently under maintenance</h1>
+        <p>We apologize for any inconvenience. We are currently working on improving our site and will be back up and running as soon as possible.</p>
+        <p>Thank you for your patience.</p>
+      </div>
+    </div>
+  );
+};
+export default Maintenance;
